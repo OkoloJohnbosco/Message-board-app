358cfaa0c005020d94b59cd30459e5bce1998593
Okolo Johnbosco <chukwuemekaokolo233@gmail.com>
2022-09-29 23:27:35 +0100

    improved and added new tests

--- posts/tests.py
+++ posts/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from .models import Post
+from django.urls import reverse
 
 # Create your tests here.
 # To run our test > python manage.py test
@@ -15,3 +16,22 @@ class PostTests(TestCase):
 
     def test_model_content(self):
         self.assertEqual(self.post.text, "This is a test")
+
+    def test_url_exist_at_correct_location(self):
+        res = self.client.get("/")
+        # check if it was fetched successfully
+        self.assertEqual(res.status_code, 200)
+        # self.asserEqual checks if the first arg is equal to the second arg
+
+    def test_url_available_by_name(self):
+        res = self.client.get(reverse("home"))
+        self.assertEqual(res.status_code, 200)
+
+    def test_template_name_correct(self):
+        res = self.client.get(reverse("home"))
+        # checks for the template rendered on that route
+        self.assertTemplateUsed(res, "home.html")
+    
+    def test_tempate_content(self):
+        res = self.client.get(reverse("home"))
+        self.assertContains(res, "Message board homepage")
