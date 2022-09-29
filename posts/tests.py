28167c298bde92a4adb42165d9196da152f81bfb
Okolo Johnbosco <chukwuemekaokolo233@gmail.com>
2022-09-29 23:38:20 +0100

    turn our unit tests into one integration test

--- posts/tests.py
+++ posts/tests.py
@@ -17,21 +17,32 @@ class PostTests(TestCase):
     def test_model_content(self):
         self.assertEqual(self.post.text, "This is a test")
 
+    # 2
     def test_url_exist_at_correct_location(self):
         res = self.client.get("/")
         # check if it was fetched successfully
         self.assertEqual(res.status_code, 200)
         # self.asserEqual checks if the first arg is equal to the second arg
 
+    # 3
     def test_url_available_by_name(self):
         res = self.client.get(reverse("home"))
         self.assertEqual(res.status_code, 200)
 
+    # 4
     def test_template_name_correct(self):
         res = self.client.get(reverse("home"))
         # checks for the template rendered on that route
         self.assertTemplateUsed(res, "home.html")
-    
+
+    # 5
     def test_tempate_content(self):
         res = self.client.get(reverse("home"))
-        self.assertContains(res, "Message board homepage")
+        self.assertContains(res, "This is a test")
+
+    # We can combine all the test above (from 2-5)  to one test
+    def test_homepage(self):
+        res = self.client.get(reverse("home"))
+        self.assertEqual(res.status_code, 200)
+        self.assertTemplateUsed(res, "home.html")
+        self.assertContains(res, "This is a test")
